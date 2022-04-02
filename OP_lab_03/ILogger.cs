b474938d6bc2cb3ba6dcf519bc8d1906a4d376a0
krzysztof.gasienica <krzysztof.gasienica@PK5-10>
2022-04-02 12:49:03 +0200

    Dodaj pliki projektów.

---
+++ OP_lab_03/ILogger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OP_lab_03
+{
+    public interface ILogger : IDisposable
+    {
+        void Log(params String[] messages);
+    }
+}
---
+++ OP_lab_03/SocketLogger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OP_lab_03
+{
+    class SocketLogger : ClientSocket
+    {
+        private ClientSocket clientSocket;
+
+        public SocketLogger(string host, int port) : base(host,port)
+        {
+
+        }
+
+        ~SocketLogger()
+        {
+            this.Dispose(false);
+        }
+    }
+}
